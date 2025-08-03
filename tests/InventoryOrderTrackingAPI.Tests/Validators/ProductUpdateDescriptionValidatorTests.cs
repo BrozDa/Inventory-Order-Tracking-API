e6fcf1fbcb09a678196961be41b1f1abe7e9df8d
Brozda <daniel.brozik@gmail.com>
2025-08-03 21:23:14 +0200

    feat: Add tests for update name/ descr validators

---
+++ tests/InventoryOrderTrackingAPI.Tests/Validators/ProductUpdateDescriptionValidatorTests.cs
@@ -0,0 +1,95 @@
+﻿using Inventory_Order_Tracking.API.Dtos;
+using Inventory_Order_Tracking.API.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.API.Tests.Validators
+{
+    public class ProductUpdateDescriptionValidatorTests
+    {
+        private readonly ProductUpdateDescriptionValidator _sut;
+
+        public ProductUpdateDescriptionValidatorTests()
+        {
+            _sut = new();
+        }
+        [Fact]
+        public void Validate_ValueNull_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateDescriptionDto
+            {
+                Description = null
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValueEmpty_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateDescriptionDto
+            {
+                Description = string.Empty,
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_TooLong_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateDescriptionDto
+            {
+                Description = "This description is too long, one would consider this very unnecessary",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValueWithSpecialChars_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateDescriptionDto
+            {
+                Description = "$pec!4l Description#",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValidValue_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateDescriptionDto
+            {
+                Description = "Top-Product Ultra 3000",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.True(result.IsValid);
+        }
+    }
+}
---
+++ tests/InventoryOrderTrackingAPI.Tests/Validators/ProductUpdateNameValidatorTests.cs
@@ -0,0 +1,96 @@
+﻿using Inventory_Order_Tracking.API.Dtos;
+using Inventory_Order_Tracking.API.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.API.Tests.Validators
+{
+    public class ProductUpdateNameValidatorTests
+    {
+        private readonly ProductUpdateNameValidator _sut;
+
+        public ProductUpdateNameValidatorTests()
+        {
+            _sut = new();
+        }
+        [Fact]
+        public void Validate_ValueNull_ReturnsFalse() 
+        {
+            //arrange
+            var updatedName = new ProductUpdateNameDto
+            {
+                Name = null
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValueEmpty_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateNameDto
+            {
+                Name = string.Empty,
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_TooLong_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateNameDto
+            {
+                Name = "This is name is too long, one would consider this very unnecessary",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValueWithSpecialChars_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateNameDto
+            {
+                Name = "$pec!4l Name#",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Validate_ValidValue_ReturnsFalse()
+        {
+            //arrange
+            var updatedName = new ProductUpdateNameDto
+            {
+                Name = "Top-Product Ultra 3000",
+            };
+            //act
+
+            var result = _sut.Validate(updatedName);
+            //assert
+
+            Assert.True(result.IsValid);
+        }
+
+    }
+}
