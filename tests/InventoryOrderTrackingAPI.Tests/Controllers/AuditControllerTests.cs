ad10ec77740b2d780a091d8fe943d86c8e623191
Brozda <daniel.brozik@gmail.com>
2025-08-05 13:24:11 +0200

    feat: Add Audit log controller, service tests

---
+++ tests/InventoryOrderTrackingAPI.Tests/Controllers/AuditControllerTests.cs
@@ -0,0 +1,109 @@
+﻿using Inventory_Order_Tracking.API.Controllers;
+using Inventory_Order_Tracking.API.Dtos;
+using Inventory_Order_Tracking.API.Services.Interfaces;
+using Inventory_Order_Tracking.API.Services.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.API.Tests.Controllers
+{
+    public class AuditControllerTests
+    {
+        private readonly AuditController _sut;
+        private readonly Mock<IAuditService> _auditServiceMock = new();
+        public AuditControllerTests()
+        {
+            _sut = new AuditController(_auditServiceMock.Object);
+        }
+        [Fact]
+        public async Task GetAllLogs_FailedRequest_ReturnsOtherThanOK() 
+        {
+            //arrange
+            var serviceResult = ServiceResult<List<AuditLogDto>>.BadRequest("Test failure");
+            _auditServiceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllLogs();
+            //assert
+            Assert.IsNotType<OkObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetAllLogs_SuccessfulRequest_ReturnsCreated()
+        {
+            //arrange
+            var logs = new List<AuditLogDto>();
+            var serviceResult = ServiceResult<List<AuditLogDto>>.Ok(logs);
+            _auditServiceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllLogs();
+            //assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetAllForUser_FailedRequest_ReturnsOtherThanOK()
+        {
+            //arrange
+            var userId = Guid.NewGuid();
+            var serviceResult = ServiceResult<List<AuditLogDto>>.BadRequest("Test failure");
+
+            _auditServiceMock.Setup(s => s.GetAllForUser(userId)).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllForUser(userId);
+            //assert
+            Assert.IsNotType<OkObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetAllForUser_SuccessfulRequest_ReturnsCreated()
+        {
+            //arrange
+            var userId = Guid.NewGuid();
+            var logs = new List<AuditLogDto>();
+            var serviceResult = ServiceResult<List<AuditLogDto>>.Ok(logs);
+
+            _auditServiceMock.Setup(s => s.GetAllForUser(userId)).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllForUser(userId);
+            //assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetAllForDate_FailedRequest_ReturnsOtherThanOK()
+        {
+            //arrange
+            var date = DateTime.UtcNow;
+            var serviceResult = ServiceResult<List<AuditLogDto>>.BadRequest("Test failure");
+
+            _auditServiceMock.Setup(s => s.GetAllForDate(date)).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllForDate(date);
+            //assert
+            Assert.IsNotType<OkObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetAllForDate_SuccessfulRequest_ReturnsCreated()
+        {
+            //arrange
+            var date = DateTime.UtcNow;
+            var logs = new List<AuditLogDto>();
+            var serviceResult = ServiceResult<List<AuditLogDto>>.Ok(logs);
+
+            _auditServiceMock.Setup(s => s.GetAllForDate(date)).ReturnsAsync(serviceResult);
+
+            //act
+            var result = await _sut.GetAllForDate(date);
+            //assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+    }
+}
---
+++ tests/InventoryOrderTrackingAPI.Tests/Services/AuditServiceTests.cs
@@ -0,0 +1,219 @@
+﻿using Inventory_Order_Tracking.API.Models;
+using Inventory_Order_Tracking.API.Repository.Interfaces;
+using Inventory_Order_Tracking.API.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.SqlServer.Server;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.API.Tests.Services
+{
+    public class AuditServiceTests
+    {
+        private readonly AuditService _sut;
+        private readonly Mock<IAuditLogRepository> _auditLogRepository = new();
+        private readonly Mock<IUserRepository> _userRepository = new();
+        private readonly Mock<ILogger<AuditService>> _loggerMock = new();
+        public AuditServiceTests()
+        {
+            _sut = new(_auditLogRepository.Object, _userRepository.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_RepoThrowsException_ReturnsInternalServerErrorAndLogsError() 
+        {
+            //arrange
+
+            _auditLogRepository.Setup(ar => ar.GetAllAuditLogsAsync()).Throws(() => new Exception("Test exception"));
+            //act
+
+            var result = await _sut.GetAllAsync();
+            //assert
+            Assert.False(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+            Assert.Equal("Failed to fetch logs", result.ErrorMessage);
+
+            _loggerMock.Verify(
+                x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("[AuditService][GetAllAsync] Unhandled Exception has occured")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+        [Fact]
+        public async Task GetAllAsync_ValidFlow_ReturnsOkWithLogs()
+        {
+            //arrange
+            var logs = new List<AuditLog>();
+            _auditLogRepository.Setup(ar => ar.GetAllAuditLogsAsync()).ReturnsAsync(logs);
+            //act
+
+            var result = await _sut.GetAllAsync();
+            //assert
+            Assert.True(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.NotNull(logs);
+        }
+
+        [Fact]
+        public async Task GetAllForDate_RepoThrowsException_ReturnsInternalServerErrorAndLogsError()
+        {
+            //arrange
+            var date = DateTime.UtcNow.AddDays(-2);
+            _auditLogRepository.Setup(ar => ar.GetAllForDateAsync(date)).Throws(() => new Exception("Test exception"));
+            //act
+
+            var result = await _sut.GetAllForDate(date);
+
+            //assert
+            Assert.False(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+            Assert.Equal("Failed to fetch logs", result.ErrorMessage);
+
+            _loggerMock.Verify(
+                x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("[AuditService][GetAllForDate] Unhandled Exception has occured")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+        [Fact]
+        public async Task GetAllForDate_DateInFuture_ReturnBadRequestAndLogsWarning()
+        {
+            //arrange
+            var date = DateTime.UtcNow.AddDays(2);
+            var logs = new List<AuditLog>();
+            _auditLogRepository.Setup(ar => ar.GetAllForDateAsync(date)).ReturnsAsync(logs);
+            //act
+
+            var result = await _sut.GetAllForDate(date);
+
+            //assert
+            Assert.False(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.Equal("Cannot get logs for future date", result.ErrorMessage);
+
+            _loggerMock.Verify(
+                x => x.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("[AuditService][GetAllForDate] Attempt to gather logs for future date")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+        [Fact]  
+        public async Task GetAllForDate_SameDate_ReturnsOkWithLogs()
+        {
+            //arrange
+            var date = DateTime.UtcNow;
+            var logs = new List<AuditLog>();
+            _auditLogRepository.Setup(ar => ar.GetAllForDateAsync(date)).ReturnsAsync(logs);
+            //act
+
+            var result = await _sut.GetAllForDate(date);
+
+            //assert
+            Assert.True(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.NotNull(logs);
+        }
+        [Fact]
+        public async Task GetAllForDate_PastDate_ReturnsOkWithLogs()
+        {
+            //arrange
+            var date = DateTime.UtcNow.AddDays(-2);
+            var logs = new List<AuditLog>();
+            _auditLogRepository.Setup(ar => ar.GetAllForDateAsync(date)).ReturnsAsync(logs);
+            //act
+
+            var result = await _sut.GetAllForDate(date);
+
+            //assert
+            Assert.True(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.NotNull(logs);
+        }
+
+        [Fact]
+        public async Task GetAllForUser_RepoThrowsException_ReturnsInternalServerErrorAndLogsError()
+        {
+            //arrange
+            var userId = Guid.NewGuid();
+            
+            _userRepository.Setup(ur => ur.IdExists(userId)).ReturnsAsync(true);
+            _auditLogRepository.Setup(ar => ar.GetAllForUserAsync(userId)).Throws(() => new Exception("Test exception"));
+            //act
+
+            var result = await _sut.GetAllForUser(userId);
+
+            //assert
+            Assert.False(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+            Assert.Equal("Failed to fetch logs", result.ErrorMessage);
+
+            _loggerMock.Verify(
+                x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("[AuditService][GetAllForUser] Unhandled Exception has occured")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+        [Fact]
+        public async Task GetAllForUser_UserDoesNotExist_ReturnBadRequestAndLogsWarning()
+        {
+            //arrange
+            var userId = Guid.NewGuid();
+            var logs = new List<AuditLog>();
+
+            _userRepository.Setup(ur => ur.IdExists(userId)).ReturnsAsync(false);
+            _auditLogRepository.Setup(ar => ar.GetAllForUserAsync(userId)).ReturnsAsync(logs);
+
+            //act
+            var result = await _sut.GetAllForUser(userId);
+
+            //assert
+            Assert.False(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.Equal("User with provided Id does not exist", result.ErrorMessage);
+
+            _loggerMock.Verify(
+                x => x.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString().Contains("[AuditService][GetAllForUser] Attempt to gather logs for non existing user")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+                Times.Once);
+        }
+        [Fact]
+        public async Task GetAllForUser_ValidFlow_ReturnsOkWithLogs()
+        {
+            //arrange
+            var userId = Guid.NewGuid();
+            var logs = new List<AuditLog>();
+
+            _userRepository.Setup(ur => ur.IdExists(userId)).ReturnsAsync(true);
+            _auditLogRepository.Setup(ar => ar.GetAllForUserAsync(userId)).ReturnsAsync(logs);
+
+            //act
+            var result = await _sut.GetAllForUser(userId);
+
+            //assert
+            Assert.True(result.IsSuccessful);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.NotNull(logs);
+        }
+    }
+}
